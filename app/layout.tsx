ed8e45d4c72c191222a5ad5be1046ab8d78c3802
GomPangEEE <wlstj0568@daum.net>
2026-08-28 23:06:37 +0900

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,3 @@
+import './globals.css'
+export const metadata={title:'수달 노래책',description:'스푼 아이돌 대수달 노래책'}
+export default function RootLayout({children}:{children:React.ReactNode}){return <html lang="ko"><body>{children}</body></html>}
